dd3bb318d09bbe73284ed7b700a41817bbb4af60
Leonardo Lino <leonardolino96@gmail.com>
2019-07-11 19:00:57 -0300

    Testes iniciais com os movimentos possíveis das Pecas

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
@@ -19,5 +19,7 @@ namespace tabuleiro {
         public void incrementarQteMovimentos() {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
